e5b95b4d69e55a7e31210019384122c2dfa9de73
alegayndra <alegayndra@gmail.com>
2021-02-16 21:13:22 -0600

    Factorized code

--- Tarea 1/main.rs
+++ Tarea 1/main.rs
@@ -1,11 +1,10 @@
 mod stack;
 
-fn main() {
-    println!("aaa");
-    println!("{}", stack::STACK_SIZE);
+fn pruebas_stack() {
+    println!("\n----- Iniciando pruebas Stack -----\n");
 
     let valores: [i32; stack::STACK_SIZE] = [0; 500];
-
+    
     let mut stack_local = stack::Stack::new(valores);
     stack_local.push(32);
     stack_local.push(3);
@@ -18,4 +17,10 @@ fn main() {
     stack_local.pop();
     stack_local.pop();
     println!("empty: {}", stack_local.is_empty());
+
+    println!("\n----- Terminando pruebas Stack -----\n");
+}
+
+fn main() {
+    pruebas_stack();
 }
